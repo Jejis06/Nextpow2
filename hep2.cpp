6b89f3c84b49238696e033ccd474281fe19c5cb5
Jejis06 <wertex3233@gmail.com>
2023-03-13 21:43:38 +0100

    added codes

---
+++ hep2.cpp
@@ -0,0 +1,9 @@
+int hep2(unsigned int x){
+        x--;
+        x |= (x >> 1);
+        x |= (x >> 2);
+        x |= (x >> 4);
+        x |= (x >> 8);
+        x |= (x >> 16);
+        return x+1;
+}
